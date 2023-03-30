50954d8d192d174c7aa2662ed279e38c60a09662
essaberYahya <essaber.yahya19@gmail.com>
2023-03-29 17:35:10 -0700

    0 strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src) {
+    char *ptr = dest;
+    while (*ptr != '\0') {
+        ptr++;
+    }
+    while (*src != '\0') {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
+    *ptr = '\0';
+    return dest;
+}
+
+int main() {
+    char str1[20] = "hello";
+    char str2[] = "world";
+    printf("%s\n", _strcat(str1, str2));
+    return 0;
+}
