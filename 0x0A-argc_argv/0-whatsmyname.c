5e68c143f746f095c8736312fe175627cc9a9d3b
essaberYahya <essaber.yahya19@gmail.com>
2023-06-14 15:18:45 -0700

    what s my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Editor : Yahya ESSABER
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
+
+
