a79452efdc951987f6c638d042f5e0e2105dd2d5
essaberYahya <essaber.yahya19@gmail.com>
2023-06-20 16:10:08 -0700

    commit files for preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * Editor : Yahya ESSABER
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
+
